fedb0dde40eefebc29b33678b1b9bf751b0b2d68
Bogdan Ailincai <bogdan.ailincai@softvision.ro>
2016-04-04 14:03:07 +0300

    set initial state / localStorage

--- app/jsx/app.jsx
+++ app/jsx/app.jsx
@@ -6,12 +6,27 @@ import Edit from './edit';
 import Present from './present';
 
 ((function() {
-	// TODO: get info from localStorage & set the initial state on App
+	let state = JSON.parse(localStorage.getItem('state'));
+	if (!state) {
+		state = {
+			'view': 'edit',
+			'currentSlide': 0,
+			'sidebar': 'controls',
+			'slides': [
+				{
+					'type': 'titleText',
+					'title': 'Slide title',
+					'text': 'Slide text'
+				}
+			]
+		};
+		localStorage.setItem('state', JSON.stringify(state));
+	}
 	const AppElement = document.createElement('div');
 	document.body.appendChild(AppElement);
 	const App = React.createClass({
 		getInitialState() {
-			return {};
+			return state;
 		},
 		render() {
 			return <div className = 'component app'>

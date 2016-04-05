ccfaf78dc91ced0dc914ddff1f6d8f3ec2758a35
Bogdan Ailincai <bogdan.ailincai@softvision.ro>
2016-04-05 14:08:01 +0300

    prev & next slide initial

--- app/jsx/controls/next-slide.jsx
+++ app/jsx/controls/next-slide.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const NextSlide = React.createClass({
+	clickHandler() {
+		this.props.updateAppState({
+			'currentSlide': this.props.state.currentSlide + 1
+		});
+	},
 	render() {
-		return <div className = 'component next-slide'>
+		return <div className = 'component next-slide' onClick = {this.clickHandler}>
 			NextSlide
 		</div>;
 	}
--- app/jsx/controls/prev-slide.jsx
+++ app/jsx/controls/prev-slide.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const PrevSlide = React.createClass({
+	clickHandler() {
+		this.props.updateAppState({
+			'currentSlide': this.props.state.currentSlide - 1
+		});
+	},
 	render() {
-		return <div className = 'component prev-slide'>
+		return <div className = 'component prev-slide' onClick = {this.clickHandler}>
 			PrevSlide
 		</div>;
 	}

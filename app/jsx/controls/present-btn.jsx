00b6ef50af5a015d625f7911f1df882ff6b6767f
Bogdan Ailincai <bogdan.ailincai@softvision.ro>
2016-04-06 09:46:11 +0300

    change state on present btn click

--- app/jsx/controls/present-btn.jsx
+++ app/jsx/controls/present-btn.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const PresentBtn = React.createClass({
+	clickHandler() {
+		this.props.updateAppState({
+			'view': 'present',
+			'currentSlide': 0
+		});
+	},
 	render() {
-		return <div className = 'component present-btn'>
+		return <div className = 'component present-btn' onClick = {this.clickHandler}>
 		</div>;
 	}
 });

3e73d4b8f59f999d59209e5d6741e6621933e215
Bogdan Ailincai <bogdan.ailincai@softvision.ro>
2016-04-08 17:23:15 +0300

    remove clog

--- app/scripts/current-slide/text.tsx
+++ app/scripts/current-slide/text.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 class Text extends React.Component<IGenericProps, IGenericState> {
 	changeHandler(e: React.SyntheticEvent): void {
-		console.log(e.target);
 		let slides: ISlide[] = this.props.state.slides;
 		slides[this.props.state.currentSlide].text = (e.target as HTMLInputElement).value;
 		this.props.updateAppState(slides);

3c2d895470c89a9e0bc297aea1660f943d4fdf55
Bogdan Ailincai <bogdan.ailincai.9@gmail.com>
2017-02-01 16:48:28 +0200

    remove unnecessary IIFE

--- app/scripts/app.tsx
+++ app/scripts/app.tsx
@@ -11,50 +11,49 @@ import Edit from './edit';
 import Present from './present';
 import {defaultState} from './defaultState';
 
-(function(): void {
-	const appElement: Element = document.createElement('div');
-	document.body.appendChild(appElement);
 
-	let state: IAppState = JSON.parse(localStorage.getItem('state'));
-	if (!state) {
-		state = defaultState;
-		localStorage.setItem('state', JSON.stringify(state));
+const appElement: Element = document.createElement('div');
+document.body.appendChild(appElement);
+
+let state: IAppState = JSON.parse(localStorage.getItem('state'));
+if (!state) {
+	state = defaultState;
+	localStorage.setItem('state', JSON.stringify(state));
+}
+
+class App extends React.Component<{}, IAppState> {
+
+	constructor(props: {}) {
+		super(props);
+		this.state = state;
+		this.updateAppState = this.updateAppState.bind(this);
 	}
 
-	class App extends React.Component<{}, IAppState> {
+	updateAppState(newState: IAppState): void {
+		this.setState(
+			newState,
+			() => localStorage.setItem('state', JSON.stringify(this.state))
+		);
+	}
 
-		constructor(props: {}) {
-			super(props);
-			this.state = state;
-			this.updateAppState = this.updateAppState.bind(this);
-		}
-
-		updateAppState(newState: IAppState): void {
-			this.setState(
-				newState,
-				() => localStorage.setItem('state', JSON.stringify(this.state))
-			);
-		}
-
-		render(): React.ReactElement<HTMLDivElement> {
-			return <div className = 'component app'>
-				{
-					this.state.view === 'edit' ? <Header /> : null
+	render(): React.ReactElement<HTMLDivElement> {
+		return <div className = 'component app'>
+			{
+				this.state.view === 'edit' ? <Header /> : null
+			}
+			{
+				this.state.view === 'edit' ?
+					<Edit
+						state = {this.state}
+						updateAppState = {this.updateAppState}
+					/> :
+					<Present
+						state = {this.state}
+						updateAppState = {this.updateAppState}
+					/>
 				}
-				{
-					this.state.view === 'edit' ?
-						<Edit
-							state = {this.state}
-							updateAppState = {this.updateAppState}
-						/> :
-						<Present
-							state = {this.state}
-							updateAppState = {this.updateAppState}
-						/>
-					}
-			</div>;
-		}
+		</div>;
 	}
+}
 
-	render(<App/>, appElement);
-})();
+render(<App/>, appElement);

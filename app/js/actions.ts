c85432fdbacdfedd5fd1e70040c78442606a7620
Bogdan Ailincai <bogdan.ailincai.9@gmail.com>
2017-07-27 00:04:15 -0700

    edit, present actions

--- app/js/actions.ts
+++ app/js/actions.ts
@@ -1,8 +1,68 @@
 import * as actionTypes from './actionTypes';
 
+export const edit: any = () => {
+  return {
+    type: actionTypes.EDIT
+  };
+};
+
+export const present: any = () => {
+  return {
+    type: actionTypes.PRESENT
+  };
+};
+
+export const prevSlide: any = () => {
+  return {
+    type: actionTypes.PREV_SLIDE
+  };
+};
+
+export const nextSlide: any = () => {
+  return {
+    type: actionTypes.NEXT_SLIDE
+  };
+};
+
+export const removeSlide: any = () => {
+  return {
+    type: actionTypes.REMOVE_SLIDE
+  };
+};
+
+export const addSlide: any = () => {
+  return {
+    type: actionTypes.ADD_SLIDE
+  };
+};
+
+export const chooseSlide: any = () => {
+  return {
+    type: actionTypes.CHOOSE_SLIDE
+  };
+};
+
+export const exitChooseSlide: any = () => {
+  return {
+    type: actionTypes.EXIT_CHOOSE_SLIDE
+  };
+};
+
+export const changeTitle: any = () => {
+  return {
+    type: actionTypes.CHANGE_TITLE
+  };
+};
+
 export const changeText: any = (text: string) => {
   return {
     type: actionTypes.CHANGE_TEXT,
     text
+  };
+};
+
+export const changePic: any = () => {
+  return {
+    type: actionTypes.CHANGE_PIC
   };
 };
--- app/js/controls/present-btn.tsx
+++ app/js/controls/present-btn.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
+import { connect, MapDispatchToProps } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-import * as actionTypes from '../actionTypes';
+import * as actions from '../actions';
 
 class PresentBtn extends React.Component<IGenericProps, {}> {
-  static contextTypes: React.ValidationMap<any> = {
-    store: React.PropTypes.object
-  };
-
   clickHandler(): void {
-    this.context.store.dispatch({
-      type: actionTypes.PRESENT
-    });
+    (this.props as any).actions.present();
   }
 
   render(): React.ReactElement<HTMLDivElement> {
@@ -25,4 +21,8 @@ class PresentBtn extends React.Component<IGenericProps, {}> {
   }
 }
 
-export default PresentBtn;
+const mapDispatchToProps: MapDispatchToProps<any, any> = (dispatch: any) => ({
+  actions: bindActionCreators(actions as any, dispatch)
+});
+
+export default connect(null, mapDispatchToProps)(PresentBtn);

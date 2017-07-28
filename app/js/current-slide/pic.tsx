96479c3e410ace2c994c002d562c5ed36fa7e021
Bogdan Ailincai <bogdan.ailincai.9@gmail.com>
2017-07-28 16:07:08 -0700

    curate pic.tsx of any uses

--- app/js/current-slide/pic.tsx
+++ app/js/current-slide/pic.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import { connect, MapStateToProps, MapDispatchToProps } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { bindActionCreators, ActionCreatorsMapObject, Dispatch } from 'redux';
 
 import { actions } from '../actions';
 
-class Pic extends React.Component<IAppState, {}> {
+interface IStateProps {
+  slides: ISlide[];
+  currentSlideIndex: number;
+}
+
+interface IDispatchProps {
+  actions: ActionCreatorsMapObject;
+}
+
+class Pic extends React.Component<IStateProps & IDispatchProps, {}> {
   changeHandler(e: React.SyntheticEvent<{}>): void {
-    (this.props as any).actions.changePic((e.target as HTMLInputElement).value);
+    this.props.actions.changePic((e.target as HTMLInputElement).value);
   }
 
   render(): React.ReactElement<HTMLDivElement> {
@@ -21,15 +30,15 @@ class Pic extends React.Component<IAppState, {}> {
   }
 }
 
-const mapStateToProps: MapStateToProps<IAppState, any> = (state: IAppState) => {
+const mapStateToProps: MapStateToProps<IStateProps, {}> = (state: IAppState) => {
   return {
     currentSlideIndex: state.currentSlideIndex,
     slides: state.slides
   };
 };
 
-const mapDispatchToProps: MapDispatchToProps<any, any> = (dispatch: any) => ({
-  actions: bindActionCreators(actions as any, dispatch)
+const mapDispatchToProps: MapDispatchToProps<IDispatchProps, {}> = (dispatch: Dispatch<IDispatchProps>) => ({
+  actions: bindActionCreators(actions, dispatch)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Pic);
